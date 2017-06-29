5dde4ac8f3cffcdce63fba870e684f447d7e03d6
Josh Bird <joshua.thomas.bird@gmail.com>
2017-06-29 10:40:59 -0400

    Use env for STATIC_ROOT

--- jirawebhooks/settings.py
+++ jirawebhooks/settings.py
@@ -118,5 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = '/srv/'
+STATIC_ROOT = os.environ['STATIC_ROOT']
 STATIC_URL = '/static/'
