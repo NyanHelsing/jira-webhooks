d07fda3ce799b2235c161c7fe0653e84ee86e58b
Josh Bird <joshua.thomas.bird@gmail.com>
2017-06-29 10:00:50 -0400

    update settings

--- jirawebhooks/settings.py
+++ jirawebhooks/settings.py
@@ -118,6 +118,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
-
+STATIC_ROOT = '/srv/'
 STATIC_URL = '/static/'
